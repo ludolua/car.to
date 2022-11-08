48a04bd6a191dab17f98eeecb184b167c5918e7e
Luiz Eduardo <87837854+ludolua@users.noreply.github.com>
2022-11-07 23:11:33 -0300

    aaaaaaaa

--- car.to-main.py
+++ car.to-main.py
@@ -2,20 +2,22 @@ import random
 import os
 import json
 
-with open("cars.json", encoding='utf-8') as meu_json:
-    dados = json.load(meu_json)
-
-
+with open("cars.json", encoding='utf8') as meu_json:
+    data = json.load(meu_json)
 
 nome = input("Olá, Qual seu nome?\n")
 
+sim = f"{nome}, o seu contrato atual contempla o seguinte:\n"
+não = f"{nome} deseja contratar um de nossos serviços?"
+
 print(f"Olá, {nome}\n")
-input("Possui algum contrato de aluguel de veiculo?\n")
+contrato = input("Possui algum contrato de aluguel de veiculo?\n[1] Sim\n[2] Não\n")
 
-sim = print(f"O seu contrato atual contempla o seguinte:\n")
-não = input(f"{nome} deseja contratar um de nossos serviços?")
-for i in dados:
-    print(i['Carro'])
-    print(i['Ano'])
-    print(i['Valor mensal'])
-
+if contrato == "1":
+    for i in data:
+        print(sim)
+        print(i['Modelo'])
+        print(i['Ano'])
+        print(i['Valor mensal'])
+elif contrato == "2":
+    print(não)
